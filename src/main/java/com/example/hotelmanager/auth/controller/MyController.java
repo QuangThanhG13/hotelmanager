e72264050f01240082bbe89b158dcdfdeb360551
QuangThanhG13 <vuquangthanh789@gmail.com>
2024-11-01 23:46:01 +0700

    hotel manager

--- src/main/java/com/example/hotelmanager/auth/controller/MyController.java
+++ src/main/java/com/example/hotelmanager/auth/controller/MyController.java
@@ -1,23 +1,23 @@
-//package com.example.hotelmanager.auth.controller;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class MyController {
-//
-//    @GetMapping("/admin")
-//    @PreAuthorize("hasRole('admin')")
-//    public ResponseEntity<String> adminEndpoint() {
-//        return ResponseEntity.ok("Admin content ") ;
-//    }
-//    @GetMapping("/user")
-//    @PreAuthorize("hasRole('user')")
-//    public String userEndpoint() {
-//        return "User content hung ";
-//    }
-//}
+package com.example.hotelmanager.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@RestController
+@RequestMapping("/api/v1/authorizations")
+public class MyController {
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('admin')")
+    public String getAdmin() {
+        return "Admin content thanh";
+    }
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('user')")
+    public String userEndpoint() {
+        return "User content hung ";
+    }
+}
